0fd39cf22ac3db2e0fa76bad9f995e818e4132bf
Jason Grewal <jason.grewal@student.sl.on.ca>
2026-07-22 06:13:33 -0700

    Use iostream library and calculate the sum of two numbers.
    Signed-off-by: Jason Grewal <jason.grewal@student.sl.on.ca>

---
+++ IOstream/Sum.cpp
@@ -0,0 +1,12 @@
+//
+// Created on 21.07.2026.
+//
+// use iostream and cout and cin to ask user for two numbers and return their sum
+#include <iostream>
+int main() {
+    std::cout << "Enter two numbers:" << std::endl;
+    int v1 = 0, v2 =0;
+    std::cin >> v1 >> v2;
+    std::cout << "The sum of " << v1 << " and " << v2 << " is: " << v1+v2 << std::endl;
+
+}
