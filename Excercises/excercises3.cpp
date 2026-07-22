669dbb0e914b7e9760de5c17a69678c86515fd99
Jason Grewal <jason.grewal@student.sl.on.ca>
2026-07-22 06:36:08 -0700

    Write a countdown from 10 to 0 using the -- operator.
    Signed-off-by: Jason Grewal <jason.grewal@student.sl.on.ca>

---
+++ Excercises/excercises3.cpp
@@ -0,0 +1,12 @@
+//
+// Created on 22.07.2026.
+//
+#include <iostream>
+
+int main() {
+    int num1 = 10;
+    while (num1 > -1) {
+        std::cout << num1 << std::endl;
+        num1 --;
+    }
+}
