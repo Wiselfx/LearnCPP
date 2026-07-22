830f3ef7725648f0d75508a3e768defa8ffbd70a
Jason Grewal <jason.grewal@student.sl.on.ca>
2026-07-22 06:14:33 -0700

    Simple C++ program to learn how to compile and read the status.
    Signed-off-by: Jason Grewal <jason.grewal@student.sl.on.ca>

---
+++ Prog1/Prog1.cpp
@@ -0,0 +1,9 @@
+//
+// Created on 19.07.2026.
+//
+// to compile this manually we run $ g++ -o Prog1 Prog1.cpp
+//-o Prog1 lets the compiler know what to name the output
+// ./Prog1 to run and echo $? to view returned value
+int main() {
+    return -1;
+}
