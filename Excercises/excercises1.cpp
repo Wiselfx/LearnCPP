3a89472c768384870fa047439f347411f0bd6456
Jason Grewal <jason.grewal@student.sl.on.ca>
2026-07-22 06:12:04 -0700

    Simple excercise to use iostream and to use the * operator to take two numbers as input and return their product as output.
    Signed-off-by: Jason Grewal <jason.grewal@student.sl.on.ca>

---
+++ Excercises/excercises1.cpp
@@ -0,0 +1,15 @@
+//
+// Created on 21.07.2026.
+//
+#include <iostream>
+
+int main() {
+
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Enter two numbers to be multiplied:" << std::endl;
+    int number1 = 0, number2 = 0;
+    std::cin >> number1 >> number2;
+    std::cout << "The product of " << number1 << " and " << number2 << " is: " << number1*number2 << std::endl;
+
+
+}
